e751f9ce2a6a1b4340b14be4a497a316a1503986
stefannycorsiino <132668930+stefannycorsiino@users.noreply.github.com>
2023-07-07 12:37:17 +0000

    Stefanny

--- atividade1/script.js
+++ atividade1/script.js
@@ -9,12 +9,42 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked){
             genero = 'Homem'
+            if (idade>=0 && idade<10){
+                //Criança
+                img.setAttribute('src', 'menino.png')
+            } else if (idade < 21){
+                //Jovem
+                img.setAttribute('src' , 'adolescenteH.png')
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute('src' , 'homem.png')
+            }else{
+                //Idoso
+                img.setAttribute('src' , 'idoso.png')
+            }
         } else if(fsex[1].checked) {
             genero = 'Mulher'
-
+            if (idade>=0 && idade<10){
+                //Criança
+                img.setAttribute('src' , 'menina.png')
+            } else if (idade < 21){
+                //Jovem
+                img.setAttribute('src' , 'adolescenteM.png')
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute('src' , 'mulher.png')
+            }else{
+                //Idoso 
+                img.setAttribute('src' , 'idosa.png')
+            }
         }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
 
